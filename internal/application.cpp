2bb10d68686d25e3d0e8f7620cb3f6597f0c207f
FUKADZIROH <106873114+FUKADZIROH@users.noreply.github.com>
2023-08-21 20:29:31 +0300

    application.cpp

--- internal/application.cpp
+++ internal/application.cpp
@@ -6,10 +6,18 @@ int main()
 	{
 		Point point1;
 		Point point2;
+		Point point;
 		cin >> point1;
 		cin >> point2;
-		cout << point1 << endl;
-		cout << point2 << endl;
+		point1.print();
+		point2.print();
+		cout << endl;
+		point = point1(point1,point2);
+		point.print();
+		Point* point3 = new Point(32, 64);
+		point3->print();
+		delete point3;
+
 	}
 	catch (const char* msg)
 	{
